b0274b4c98e881ef9ec18fc4d0ee87853a32168d
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-29 13:16:17 -0600

    Initialize the test and add test for instance

---
+++ test/market_test.rb
@@ -0,0 +1,18 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require './lib/vendor'
+require './lib/market'
+
+class MarketTest < Minitest::Test
+
+  def setup
+    @market = Market.new("South Pearl Street Farmers Market")
+  end
+
+  def test_it_exists
+    assert_instance_of Market, @market
+  end
+
+end
