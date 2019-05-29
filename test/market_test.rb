8c4795885934dd38bba718ac8be8c3c02593ec6a
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-29 13:55:02 -0600

    Add test for selling items from market

--- test/market_test.rb
+++ test/market_test.rb
@@ -68,4 +68,18 @@ class MarketTest < Minitest::Test
     assert_equal ({"Peaches"=>100, "Tomatoes"=>7, "Banana Nice Cream"=>50, "Peach-Raspberry Nice Cream"=>25}), @market.total_inventory
   end
 
+  def test_market_sell
+    @vendor_1.stock("Peaches", 35)
+    @vendor_1.stock("Tomatoes", 7)
+    @vendor_2.stock("Banana Nice Cream", 50)
+    @vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    @vendor_3.stock("Peaches", 65)
+
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
+
+    refute @market.sell("Peaches", 200)
+  end
+
 end
