6cb8475b158eeec19842512f2706d5ef1b5c40fc
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-29 13:05:44 -0600

    Add tests for instance and attributes

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -10,4 +10,13 @@ class VendorTest < Minitest::Test
     @vendor = Vendor.new("Rocky Mountain Fresh")
   end
 
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
+
+  def test_the_attributes
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+    assert_equal ({}), @vendor.inventory
+  end
+
 end
