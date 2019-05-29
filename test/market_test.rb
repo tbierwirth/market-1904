7e91c09e6babfa86a15611717058b340070e6196
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-29 13:21:28 -0600

    Add test for adding vendors to market

--- test/market_test.rb
+++ test/market_test.rb
@@ -9,10 +9,32 @@ class MarketTest < Minitest::Test
 
   def setup
     @market = Market.new("South Pearl Street Farmers Market")
+    @vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor_3 = Vendor.new("Palisade Peach Shack")
   end
 
   def test_it_exists
     assert_instance_of Market, @market
+  end
+
+  def test_the_attributes
+    assert_equal "South Pearl Street Farmers Market", @market.name
+    assert_equal [], @market.vendors
+  end
+
+  def test_add_vendor
+    @vendor_1.stock("Peaches", 35)
+    @vendor_1.stock("Tomatoes", 7)
+    @vendor_2.stock("Banana Nice Cream", 50)
+    @vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    @vendor_3.stock("Peaches", 65)
+
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_1)
+
+    assert_equal [@vendor_1, @vendor_2, @vendor_3], @market.vendors
   end
 
 end
